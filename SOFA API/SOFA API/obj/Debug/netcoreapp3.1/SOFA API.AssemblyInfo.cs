8ecad2916fe437330ce2197d2c4a80e0675254ec
vankcdhv <43224111+vankcdhv@users.noreply.github.com>
2021-01-10 16:02:09 +0000

    Update ignore

--- SOFA API/SOFA API/obj/Debug/netcoreapp3.1/SOFA API.AssemblyInfo.cs
+++ SOFA API/SOFA API/obj/Debug/netcoreapp3.1/SOFA API.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("5704b951-bdc3-4bec-b320-fc6c8ace18f1")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("SOFA API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
