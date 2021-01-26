f72d506d8ce22350b8ec2391ca074bfcafb570aa
vankcdhv <vank48dhv@gmail.com>
2021-01-26 10:01:25 +0700

    VanLT - API - Create structure folder

---
+++ 2. Source Code/SOFA API/SOFA API/DTO/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SOFA_API.DTO
+{
+    public class Account
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+        public Account() { }
+        public Account(int iD, string userName, string passWord)
+        {
+            ID = iD;
+            UserName = userName;
+            PassWord = passWord;
+        }
+    }
+}
---
+++ 2. Source Code/SOFA API/SOFA API/Service/AuthService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SOFA_API.Service
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthService : ControllerBase
+    {
+    }
+}
