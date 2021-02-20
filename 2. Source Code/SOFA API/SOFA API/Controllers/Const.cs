a7632fbff916e18e9a1019c740a629e06115429f
vankcdhv <vank48dhv@gmail.com>
2021-02-20 09:40:13 +0700

    VanLT - API - Modify Const

---
+++ 2. Source Code/SOFA API/SOFA API/Controllers/Const.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SOFA_API.Controllers
+{
+    public class Const
+    {
+        public static string REQUEST_CODE_SUCCESSFULLY = "SUCCESSFULLY";
+        public static string REQUEST_CODE_FAILED = "FAILED";
+    }
+}
