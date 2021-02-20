49b7542f7f1ce816f45b8b57039f854b3b431312
vankcdhv <vank48dhv@gmail.com>
2021-02-20 09:41:31 +0700

    VanLT - Android - Modify Const

--- 2. Source Code/SOFA/src/common/const.js
+++ 2. Source Code/SOFA/src/common/const.js
@@ -3,6 +3,7 @@ export const dimHeigth = 711;
 export const Horizontal = 'Horizontal';
 export const Vertical = 'Vertical';
 export const domain = 'http://139.180.214.58/TestJWT/';
+
 export const REQUEST_CODE_SUCCESSFULLY = "SUCCESSFULLY";
 export const REQUEST_CODE_FAILED = "FAILED";
 
