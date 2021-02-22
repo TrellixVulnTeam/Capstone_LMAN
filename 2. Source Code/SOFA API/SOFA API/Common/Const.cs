c6dc29b2951091bd2608a3354b89914bf083e969
vankcdhv <vank48dhv@gmail.com>
2021-02-22 16:38:34 +0700

    VanLT - API - Modify const

--- 2. Source Code/SOFA API/SOFA API/Common/Const.cs
+++ 2. Source Code/SOFA API/SOFA API/Common/Const.cs
@@ -7,18 +7,27 @@ namespace SOFA_API.Common
 {
     public class Const
     {
-        public static string SMTP_GMAIL = "smtp.gmail.com";
-        public static string GMAIL_ACCOUNT = "SOFATeam2021@gmail.com";
-        public static string GMAIL_APPLICATION_PASSWORD = "pnymsplkpljsfcqk";
+        public const string SMTP_GMAIL = "smtp.gmail.com";
+        public const string GMAIL_ACCOUNT = "SOFATeam2021@gmail.com";
+        public const string GMAIL_APPLICATION_PASSWORD = "pnymsplkpljsfcqk";
 
+        public const string MAIL_TYPE_OTP_GENERAL = "SEND_OTP";
 
+        public const int VERIFICATION_TIME_WAIT = 60;
+        public const int VERIFICATION_STATUS_MATCH = 1;
+        public const int VERIFICATION_STATUS_NOT_MATCH = 0;
+        public const int VERIFICATION_EMAIL_NOT_FOUND = 2;
+        public const int VERIFICATION_PHONE_NOT_FOUND = 3;
+        public const int VERIFICATION_ACCOUNT_NOT_FOUND = 4;
 
-        public static int VERIFICATION_TIME_WAIT = 60;
-        public static int VERIFICATION_STATUS_MATCH = 1;
-        public static int VERIFICATION_STATUS_NOT_MATCH = 0;
+        public const int VERIFICATION_METHOD_USERNAME = 1;
+        public const int VERIFICATION_METHOD_EMAIL = 2;
+        public const int VERIFICATION_METHOD_PHONE = 3;
 
-        public static string REQUEST_CODE_SUCCESSFULLY = "SUCCESSFULLY";
-        public static string REQUEST_CODE_FAILED = "FAILED";
+        public const string REQUEST_CODE_SUCCESSFULLY = "SUCCESSFULLY";
+        public const string REQUEST_CODE_FAILED = "FAILED";
+
+        
 
 
     }
--- 2. Source Code/SOFA/src/common/const.js
+++ 2. Source Code/SOFA/src/common/const.js
@@ -5,7 +5,19 @@ export const Vertical = 'Vertical';
 export const domain = 'http://139.180.214.58/TestJWT/';
 
 export const TIME_WAIT_VERIFICATION = 60;
+export const VERIFICATION_TIME_WAIT = 60;
+export const VERIFICATION_STATUS_MATCH = 1;
+export const VERIFICATION_STATUS_NOT_MATCH = 0;
+export const VERIFICATION_EMAIL_NOT_FOUND = 2;
+export const VERIFICATION_PHONE_NOT_FOUND = 3;
+export const VERIFICATION_ACCOUNT_NOT_FOUND = 4;
+
+export const VERIFICATION_METHOD_USERNAME = 1;
+export const VERIFICATION_METHOD_EMAIL = 2;
+export const VERIFICATION_METHOD_PHONE = 3;
+
 
 export const REQUEST_CODE_SUCCESSFULLY = "SUCCESSFULLY";
 export const REQUEST_CODE_FAILED = "FAILED";
+
 
