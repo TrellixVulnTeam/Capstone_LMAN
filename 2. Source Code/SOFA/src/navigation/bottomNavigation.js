3a1b9c6cd89e0fe485f92449da37c0a445fce78c
vankcdhv <vank48dhv@gmail.com>
2021-03-01 18:16:04 +0700

    VanLT - Android - Update assets

--- 2. Source Code/SOFA/src/navigation/bottomNavigation.js
+++ 2. Source Code/SOFA/src/navigation/bottomNavigation.js
@@ -65,7 +65,7 @@ export default function Navigation() {
         />
         <Tab.Screen
           name="Profile"
-          component={NotificationStack}
+          component={ProfileStack}
           options={{
             title: 'Tài khoản',
             tabBarIcon: ({color, size}) => (
