38444e97aeba5efafdea5ddf3a3dda2d6a0694be
vietdun99 <77425118+vietdun99@users.noreply.github.com>
2021-03-02 11:28:41 +0700

    DungHV - update profile

--- 2. Source Code/SOFA/src/component/profile.js
+++ 2. Source Code/SOFA/src/component/profile.js
@@ -11,11 +11,17 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as Style from '../style/style';
 import * as Const from "../common/const";
 import * as Utils from "../common/utils";
+import { AVATAR, ADDRESS_ICON, BIRTHDAY_ICON, PHONE_ICON, GENDER_ICON } from '../../image/index';
+import { TextInput } from 'react-native-gesture-handler';
+import { acc } from 'react-native-reanimated';
 
 export default class Profile extends Component{
     constructor(props) {
         super(props);
         this.state = {
+            account: {},
+            avatarUri: '',
+            token: ''
         }
     }
     getData = async (key) => {
@@ -39,10 +45,121 @@ export default class Profile extends Component{
         }
     }
 
+    getProfile(){
+        const { account } = this.state;
+        console.log('Check login');
+        this.getData('token')
+        .then(result => {
+            if(result){
+                var header = {
+                    "User-Agent": 'Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Mobile Safari/537.36',
+                    "Accept": 'application/json',
+                    "Authorization": 'Bearer ' + result.toString().substr(1, result.length - 2)
+                };
+                let url = Const.domain + 'api/profile';
+                Request.Get(url, header)
+                        .then(response => {
+                            if (response && response.code && response.code == Const.REQUEST_CODE_SUCCESSFULLY) {
+                                this.setState({ account: response });
+                                this.setState({ avatarUri: Const.assets_domain + response.avatarUri + '?time=' + new Date() });
+                                this.setState({ token: result.toString().substr(1, result.length - 2) });
+                            } else {
+                                this.props.navigation.navigate('Login')
+                            }
+                        })
+                        .catch(reason => {
+                            console.log(reason);
+                            this.props.navigation.navigate('Login')
+
+                        });
+            }else{
+                this.props.navigation.navigate('Login')
+            }
+        })
+        .catch(reason => {
+            console.log('failed');
+            this.props.navigation.navigate('Login')
+        })
+    }
+
+    formatBirthday(dob){
+        let today = new Date(dob);
+        let date=today.getDate() + "/"+ parseInt(today.getMonth()+1) +"/"+today.getFullYear();
+        return date;
+    }
+
+    formatGender(gender){
+        let sex = "Nam";
+        if(gender != true){
+            sex = "Nữ";
+        }
+        return sex;
+    }
+
+    componentWillUnmount() {
+        //this._unsubcribe();
+    }
+
+    componentDidMount(){
+        this.getProfile();
+    }
+
     render(){
+        const { account, avatarUri} = this.state;
         return (
             <View>
-                <Text>Profile</Text>
+                <StatusBar hidden={false} backgroundColor='#fbb897' />
+                <LinearGradient colors={['#fbb897','#ff8683']}>
+                    <View style={Style.profile.firstHeader}>
+                        <Image 
+                        source={avatarUri ? { uri: avatarUri } : AVATAR}
+                        resizeMode={"cover"}
+                        style={Style.profile.image} />
+                        <Text style={Style.profile.userName}>{account.firstName + ' ' + account.lastName}</Text>
+                        <Text style={Style.profile.email}>{account.email}</Text>
+                        <View style={Style.profile.basicInfo}>
+                            <Text style={Style.profile.basicSmallInfo}>{account.postNumber}{"\n"}Posts</Text>
+                            <Text style={Style.profile.basicSmallInfo}>{account.followerNumber}{"\n"}Followers</Text>
+                        </View>
+                        <View style={Style.profile.button}>
+                            <Button style={Style.profile.singleButton} color= '#ff7878' title="Update profile"/>
+                            <View style={{flex: 0.2}}></View>
+                            <Button style={Style.profile.singleButton} color= '#ff7878' title="View balance"/>
+                        </View>
+                    </View>
+                </LinearGradient>                       
+                <View style={Style.profile.information}>    
+                    <Image source={ADDRESS_ICON} style={Style.profile.info_icon} />                   
+                    <Text style={Style.profile.info_text}>
+                        Đến từ <Text style={{fontWeight: "bold"}}>
+                            {account.address}
+                        </Text>                       
+                    </Text>                   
+                </View>
+                <View style={Style.profile.information}>    
+                    <Image source={BIRTHDAY_ICON} style={Style.profile.info_icon} />                   
+                    <Text style={Style.profile.info_text}>
+                        Ngày sinh <Text style={{fontWeight: "bold"}}>
+                            {this.formatBirthday(account.dob)}
+                        </Text>                       
+                    </Text>                   
+                </View>
+                <View style={Style.profile.information}>    
+                    <Image source={PHONE_ICON} style={Style.profile.info_icon} />                   
+                    <Text style={Style.profile.info_text}>
+                        Điện thoại <Text style={{fontWeight: "bold"}}>
+                            {account.phone}
+                        </Text>                       
+                    </Text>                   
+                </View>
+                <View style={Style.profile.information}>    
+                    <Image source={GENDER_ICON} style={Style.profile.info_icon} />                   
+                    <Text style={Style.profile.info_text}>
+                        Giới tính <Text style={{fontWeight: "bold"}}>
+                            {this.formatGender(account.gender)}
+                        </Text>                       
+                    </Text>                   
+                </View>
             </View>
         )
     }
--- 2. Source Code/SOFA/src/style/style.js
+++ 2. Source Code/SOFA/src/style/style.js
@@ -27,3 +27,86 @@ export const common = StyleSheet.create({
 export const login = StyleSheet.create({
   container: { flex: 1 },
 })
+
+export const profile = StyleSheet.create({
+  userName: {
+    fontSize: Utils.scale(30, Const.Horizontal),
+    color: 'white',
+    fontFamily: 'OpenSansCondensed-Light',
+    paddingTop: Utils.scale(10, Const.Vertical),
+  },
+
+  email: {
+    fontSize: Utils.scale(15, Const.Horizontal),
+    color: 'white',
+    fontFamily: 'OpenSansCondensed-Light',
+  },
+
+  firstHeader: {
+    alignContent: 'center',
+    alignItems: 'center',
+    paddingTop: Utils.scale(20, Const.Vertical),
+    paddingBottom: Utils.scale(20, Const.Vertical),
+    borderBottomLeftRadius: 15,
+    borderBottomRightRadius: 15,
+  },
+
+  image: {
+    height: Utils.scale(100, Const.Vertical),
+    width: Utils.scale(110, Const.Horizontal),
+    borderRadius: Utils.scale(55, Const.Horizontal),
+    borderWidth: 2,
+    overflow: "hidden",
+    alignSelf: 'center'
+  },
+
+  basicInfo: {
+    paddingTop: Utils.scale(10, Const.Vertical),
+    flexDirection: "row"
+  },
+
+  basicSmallInfo: {
+    flex: 0.3,
+    textAlignVertical: "center",
+    textAlign: "center",
+    alignSelf: 'center',
+    color: 'white',
+    fontSize: Utils.scale(20, Const.Horizontal),   
+  }, 
+
+  button: {
+    paddingTop: Utils.scale(10, Const.Vertical),
+    flexDirection: "row",
+  },
+
+  singleButton: {
+    width: Utils.scale(130, Const.Horizontal),
+    height: Utils.scale(25, Const.Vertical),
+    flex: 0.4,
+    textAlignVertical: "center",
+    textAlign: "center",
+    alignSelf: 'center',
+    borderWidth: 1,
+    overflow: "hidden",
+    borderRadius: 10,
+  },
+
+  information: {
+    paddingTop: Utils.scale(10, Const.Vertical),
+    flexDirection: "row",
+  },
+
+  info_icon: {
+    width: Utils.scale(25, Const.Horizontal),
+    height: Utils.scale(25, Const.Vertical),
+    marginLeft: Utils.scale(10, Const.Horizontal)
+  },
+
+  info_text: {
+    fontSize: Utils.scale(17.5, Const.Horizontal), 
+    marginLeft: Utils.scale(10, Const.Horizontal),
+    color: "#ff5050"
+  }
+
+
+})
