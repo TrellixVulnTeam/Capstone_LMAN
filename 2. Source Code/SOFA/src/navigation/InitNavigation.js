3db828116fa30cfc7d1abb7f8c2018140be39d72
vienmv <vienmv.dev@gmail.com>
2021-03-04 16:52:49 +0700

    VienMV_ Fix InitNavigation 1652 040321

--- 2. Source Code/SOFA/src/navigation/InitNavigation.js
+++ 2. Source Code/SOFA/src/navigation/InitNavigation.js
@@ -5,7 +5,6 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Login from '../component/login';
 import Register from '../component/register';
 import BottomNav from '../navigation/bottomNavigation'
-import Balance from '../component/balance';
 
 const Stack = createStackNavigator();
 
@@ -16,8 +15,6 @@ export default function Navigation() {
                 <Stack.Screen name='Login' component={Login} options={{ headerShown: false }} />
                 <Stack.Screen name='BottomNav' component={BottomNav} options={{ headerShown: false }} />
                 <Stack.Screen name='Register' component={Register} options={{headerShown:false}} />
-                <Stack.Screen name='Balance' component={Balance} options={{ headerShown: false }} />
-
             </Stack.Navigator>
         </NavigationContainer>
     )
