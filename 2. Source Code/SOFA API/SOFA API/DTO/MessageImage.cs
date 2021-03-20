141f3f622963bc9a6b97ebbff41a2224c5ad9772
vietdun99 <77425118+vietdun99@users.noreply.github.com>
2021-03-20 10:46:06 +0700

    DungHV - commit DTO - MessageImage

---
+++ 2. Source Code/SOFA API/SOFA API/DTO/MessageImage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SOFA_API.DTO
+{
+    public class MessageImage
+    {
+        public int ID { get; set; }
+        public int MessageId { get; set; }
+        public string Url { get; set; }
+
+        public MessageImage()
+        {
+
+        }
+
+        public MessageImage(int iD, int messageId, string url)
+        {
+            this.ID = iD;
+            this.MessageId = messageId;
+            this.Url = url;
+        }
+
+        public MessageImage(DataRow row)
+        {
+            this.ID = (int)row["Id"];
+            this.MessageId = (int)row["MessageId"];
+            this.Url = row["Url"].ToString();
+        }
+    }
+}
