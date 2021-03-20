3268d4d9cc91e56eacb0926a7cb9c5132380374d
vietdun99 <77425118+vietdun99@users.noreply.github.com>
2021-03-20 11:33:12 +0700

    DungHV - commit something about Message

---
+++ 2. Source Code/Database/Create Procedure Get Message By ConversationId.sql
@@ -0,0 +1,10 @@
+CREATE PROCEDURE getMessageByConversationId
+@conversationId int
+AS
+BEGIN
+SELECT Message.Id, Message.FromAccountId, Message.ToAccountId, Message.Content, Message.SenderDeleted, Message.ReceiverDeleted, Message.IsRead, Message.ConversationId,Message.Time, MessageImage.Url
+  FROM dbo.Message
+  LEFT OUTER JOIN dbo.MessageImage
+  ON Message.Id = MessageImage.MessageId
+  where Message.ConversationId = @conversationId
+END
