ad4c7d25f57c62ef1244ca99c611e1c97751c692
TungNT <tungnt.997@gmail.com>
2021-03-25 09:48:10 +0700

    TungNT - Test SignalR

--- 2. Source Code/SOFA API/SOFA API/Controllers/NotificationController.cs
+++ 2. Source Code/SOFA API/SOFA API/Controllers/NotificationController.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SOFA_API.Hubs;
 using SOFA_API.Service;
 using SOFA_API.ViewModel.Notification;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,11 +16,26 @@ namespace SOFA_API.Controllers
     [ApiController]
     public class NotificationController : ControllerBase
     {
+        protected readonly IHubContext<NotificationHub> notificationHub;
+
+        public NotificationController([NotNull] IHubContext<NotificationHub> notificationHub)
+        {
+            this.notificationHub = notificationHub;
+        }
+
         [HttpGet("getnotibyid")]
         public ActionResult GetNotificationByUserID(int accountID)
         {
             ListNotificationViewModelOut listNoti = NotificationService.Instance.GetNotificationByToAccount(accountID);
+
             return Ok(listNoti);
+        }
+
+        [HttpGet("getnoti")]
+        public async Task<ActionResult> GetNotiAsync()
+        {
+            await notificationHub.Clients.User('a'.ToString()).SendAsync("NewNotification", '1');
+            return Ok();
         }
     }
 }
