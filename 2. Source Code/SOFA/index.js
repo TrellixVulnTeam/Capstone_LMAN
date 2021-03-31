af1186c1a0267f53bfe5091aa2831628f2f05eba
vankcdhv <vank48dhv@gmail.com>
2021-03-31 18:14:36 +0700

    Update index.js

--- 2. Source Code/SOFA/index.js
+++ 2. Source Code/SOFA/index.js
@@ -8,6 +8,7 @@ import Navigation from './src/navigation/InitNavigation';
 import { name as appName } from './app.json';
 import { typography } from './utils/typography';
 import PushNotification from "react-native-push-notification";
+import NotificationWSS from './src/service/NotificationWSS';
 
 typography();
 
