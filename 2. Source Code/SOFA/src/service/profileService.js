a3803a8fc2090e2ee362ca32ffaf2d7375c55822
vankcdhv <43224111+vankcdhv@users.noreply.github.com>
2021-04-03 09:08:11 +0700

    Create profileService.js

---
+++ 2. Source Code/SOFA/src/service/profileService.js
@@ -0,0 +1,54 @@
+import * as Request from "../common/request";
+import * as Const from "../common/const";
+import { getData } from "../common/utils";
+import Session from "../common/session";
+
+
+export const getProfile = () => {
+    return new Promise((resolve, reject) => {
+        getData('token')
+            .then(result => {
+                if (result) {
+                    let token = result.toString().substr(1, result.length - 2);
+                    var header = {
+                        "User-Agent": 'Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Mobile Safari/537.36',
+                        "Accept": 'application/json',
+                        "Authorization": 'Bearer ' + token,
+                    };
+                    var uri = Const.domain + 'api/profile';
+                    Request.Get(uri, header)
+                        .then(response => {
+                            Session.getInstance().account = response;
+                            Session.getInstance().token = token;
+                            resolve(response);
+                        })
+                        .catch(reason => {
+                            reject(reason);
+                        })
+                } else {
+                    reject({ code: Const.REQUEST_CODE_NOT_LOGIN })
+                }
+            })
+            .catch(reason => {
+                reject(reason);
+            })
+    })
+}
+export const getOtherProfile = (id) => {
+    return new Promise((resolve, reject) => {
+        let token = Session.getInstance().token;
+        var header = {
+            "User-Agent": 'Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Mobile Safari/537.36',
+            "Accept": 'application/json',
+            "Authorization": 'Bearer ' + token,
+        };
+        var uri = Const.domain + 'api/profile/otherprofile?id=' + id;
+        Request.Get(uri, header)
+            .then(response => {
+                resolve(response);
+            })
+            .catch(reason => {
+                reject(reason);
+            })
+    })
+}
