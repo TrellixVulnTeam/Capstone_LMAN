45b76da3f01ed32d49ad27fb517e3ecffdbce8ab
huyyhoangg0609 <53828457+huyyhoangg0609@users.noreply.github.com>
2021-04-05 16:20:22 +0700

    HoangNH - Update Backend Notification

--- 2. Source Code/SOFA API/SOFA API/Service/NotificationService.cs
+++ 2. Source Code/SOFA API/SOFA API/Service/NotificationService.cs
@@ -29,12 +29,12 @@ namespace SOFA_API.Service
 
         public NotificationService() { }
 
-        public ListNotificationViewModelOut GetNotificationByToAccount(int accountID)
+        public ListNotificationViewModelOut GetNotificationByToAccount(int accountID, int page, int rowOfPage)
         {
             ListNotificationViewModelOut listNotification = new ListNotificationViewModelOut();
             try
             {
-                listNotification = NotificationDAO.Instance.GetNotificationByToAccount(accountID);
+                listNotification = NotificationDAO.Instance.GetNotificationByToAccount(accountID, page, rowOfPage);
                 if (listNotification != null)
                 {
                     listNotification.Code = Const.REQUEST_CODE_SUCCESSFULLY;
@@ -55,12 +55,12 @@ namespace SOFA_API.Service
             return listNotification;
         }
 
-        public ListNotificationViewModelOut GetUnreadNotificationByToAccount(int accountID)
+        public ListNotificationViewModelOut GetUnreadNotificationByToAccount(int accountID, int page, int rowOfPage)
         {
             ListNotificationViewModelOut listNotification = new ListNotificationViewModelOut();
             try
             {
-                listNotification = NotificationDAO.Instance.GetUnreadNotificationByToAccount(accountID);
+                listNotification = NotificationDAO.Instance.GetUnreadNotificationByToAccount(accountID,page,rowOfPage);
                 if (listNotification != null)
                 {
                     listNotification.Code = Const.REQUEST_CODE_SUCCESSFULLY;
