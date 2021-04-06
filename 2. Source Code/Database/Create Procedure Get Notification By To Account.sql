d2209924048a949c794e0314419350b81c59f935
huyyhoangg0609 <53828457+huyyhoangg0609@users.noreply.github.com>
2021-04-06 17:29:12 +0700

    HoangNH - Update getNotificationByToAccount

--- 2. Source Code/Database/Create Procedure Get Notification By To Account.sql
+++ 2. Source Code/Database/Create Procedure Get Notification By To Account.sql
@@ -7,16 +7,17 @@ CREATE PROCEDURE getNotificationByToAccount
 @accountID int, @page INT, @rowsOfPage INT
 AS
 BEGIN
-	SELECT *
+	SELECT *, DATEDIFF(day,Notification.DateCreated,GETDATE()) AS CountDate
 	FROM dbo.[Notification]
-	WHERE ToAccount = @accountID
+	WHERE ToAccount = @accountID AND DATEDIFF(day,Notification.DateCreated,GETDATE()) < 5
 	ORDER BY IsRead ASC, DateCreated desc
 	OFFSET (@page-1)*@rowsOfPage ROWS
 	FETCH NEXT @rowsOfPage ROWS ONLY
 END
 
+SELECT DATEDIFF(day,'2021/04/05 08:00',GETDATE())
 
-EXEC getNotificationByToAccount 7,10,7
+EXEC getNotificationByToAccount 13,2,7
 
 
 
