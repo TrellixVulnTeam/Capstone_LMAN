dc5791ce805c62842158c45f484d0cde800586d4
TungNT <tungnt.997@gmail.com>
2021-04-11 23:53:11 +0700

    TungNT - Update Manager admin add topup

--- 2. Source Code/SOFAManager/src/app/management/mat-dialog-confirm/mat-dialog-confirm.component.ts
+++ 2. Source Code/SOFAManager/src/app/management/mat-dialog-confirm/mat-dialog-confirm.component.ts
@@ -8,6 +8,8 @@ import { MAT_DIALOG_DATA } from '@angular/material';
 })
 export class MatDialogConfirmComponent implements OnInit {
 
+  inputData: '';
+
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit() {
--- 2. Source Code/SOFAManager/src/app/management/user-detail/user-detail.component.ts
+++ 2. Source Code/SOFAManager/src/app/management/user-detail/user-detail.component.ts
@@ -18,6 +18,7 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class UserDetailComponent implements OnInit {
 
+  adminId = '';
   accountId = 0;
   userDetail: User = new User();
   userBalance: UserBalance = new UserBalance();
@@ -33,8 +34,10 @@ export class UserDetailComponent implements OnInit {
     private toastr: ToastrService) { }
 
   ngOnInit() {
-    this.accountId = +this.route.snapshot.paramMap.get('id');
+    let admin = JSON.parse(localStorage.getItem('user'));
+    this.adminId = admin["id"];
 
+    this.accountId = +this.route.snapshot.paramMap.get('id');
     const userDetail = this.apiService.get('user/GetUserDetailByID?id=' + this.accountId);
     const userBalance = this.apiService.get('balance/GetUserBalanceById?id=' + this.accountId);
     const userPost = this.apiService.get('post/GetPostByUserWithoutPaging?id=' + this.accountId);
@@ -56,7 +59,6 @@ export class UserDetailComponent implements OnInit {
         formData.append('accountId', id);
         let url = 'user/BanUser';
         this.apiService.post(url, formData).subscribe(response => {
-          console.log(response);
           if ((<any>response).code == CONST.REQUEST_CODE_SUCCESSFULLY) {
             this.userDetail.isActive = <any>response["isActive"];
             this.notificationSuccess("Ban người dùng thành công");
@@ -80,7 +82,6 @@ export class UserDetailComponent implements OnInit {
         formData.append('accountId', id);
         let url = 'user/UnbanUser';
         this.apiService.post(url, formData).subscribe(response => {
-          console.log(response);
           if ((<any>response).code == CONST.REQUEST_CODE_SUCCESSFULLY) {
             this.userDetail.isActive = <any>response["isActive"];
             this.notificationSuccess("Unban người dùng thành công");
@@ -95,11 +96,56 @@ export class UserDetailComponent implements OnInit {
     });
   }
 
-  onClickResetPassword(id){
+  onClickResetPassword(id) {
+    const dialogRef = this.dialog.open(MatDialogConfirmComponent,
+      { data: { title: "Đặt lại mật khẩu", content: "Bạn muốn đặt lại mật khẩu cho người dùng này?" } });
 
+    dialogRef.afterClosed().subscribe(result => {
+      if (result == true) {
+        let formData = new FormData();
+        formData.append('accountId', id);
+        let url = 'auth/admin-reset-password';
+        this.apiService.post(url, formData).subscribe(response => {
+          if ((<any>response).code == CONST.REQUEST_CODE_SUCCESSFULLY) {
+            this.notificationSuccess("Reset password thành công!");
+          }
+          else {
+
+          }
+        }, error => {
+          console.log((<any>error).code);
+        })
+      }
+    });
   }
 
-  
+  onClickAddBalance(id) {
+    const dialogRef = this.dialog.open(MatDialogConfirmComponent,
+      { width: '300px', data: { title: "Thêm balance", isGetInput: true } });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        let formData = new FormData();
+        formData.append('accountId', id);
+        formData.append('adminId', this.adminId);
+        formData.append('amount', result);
+        let url = 'balance/topUpAccount';
+        this.apiService.post(url, formData).subscribe(response => {
+          if ((<any>response).code == CONST.REQUEST_CODE_SUCCESSFULLY) {
+            this.notificationSuccess("Thêm balance thành công!");
+            this.ngOnInit();
+          }
+          else {
+
+          }
+        }, error => {
+          console.log((<any>error).code);
+        })
+      }
+    });
+  }
+
+
   notificationSuccess(notification: string) {
     this.toastr.success(notification, '', {
       timeOut: 2000, positionClass: 'toast-top-center'
