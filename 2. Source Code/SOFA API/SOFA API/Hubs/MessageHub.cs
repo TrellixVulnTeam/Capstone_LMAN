14546e49c6c11574b0cc618a96d837fc02b6dad3
vankcdhv <vank48dhv@gmail.com>
2021-04-21 08:27:34 +0000

    VanLT - Api - Create active inactive function

--- 2. Source Code/SOFA API/SOFA API/Hubs/MessageHub.cs
+++ 2. Source Code/SOFA API/SOFA API/Hubs/MessageHub.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using SOFA_API.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,20 @@ namespace SOFA_API.Hubs
 {
     public class MessageHub : Hub
     {
+        public override Task OnConnectedAsync()
+        {
+            int userID = Utils.Instance.GetUserID(Context.User.Claims);
+            Session.addUserActive(userID);
+            this.Clients.All.SendAsync("ChangeStatus", Session.ListUserActive);
+            return base.OnConnectedAsync();
+        }
 
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            int userID = Utils.Instance.GetUserID(Context.User.Claims);
+            Session.removeUserActive(userID);
+            this.Clients.All.SendAsync("ChangeStatus", Session.ListUserActive);
+            return base.OnDisconnectedAsync(exception);
+        }
     }
 }
---
+++ 2. Source Code/SOFA API/SOFA API/Hubs/Session.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SOFA_API.Hubs
+{
+    public class Session
+    {
+        public static int NumberUserActive { get; set; }
+        public static List<int> ListUserActive { get; set; }
+        public static void addUserActive(int userID)
+        {
+            if (ListUserActive.IndexOf(userID) == -1)
+            {
+                ListUserActive.Add(userID);
+            }
+        }
+        public static void removeUserActive(int userID)
+        {
+            if (ListUserActive.IndexOf(userID) != -1)
+            {
+                ListUserActive.Remove(userID);
+            }
+        }
+    }
+}
